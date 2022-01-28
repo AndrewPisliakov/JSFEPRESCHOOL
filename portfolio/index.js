56dae83fe7e23df8afcc89e2605948d4079015ab
Andrew Pisliakov <pislakov@gmail.com>
2022-01-28 13:38:02 +0300

    feat: add adaptive button

--- portfolio/index.js
+++ portfolio/index.js
@@ -14,28 +14,43 @@ elemGamburher.addEventListener('click', () => {
 const navList = document.querySelector('.nav-list');
 
 navList.addEventListener('click', () => {
-   if(mainNavbar.classList.contains('activeNavbar')) {
-       // код удаляющий активный класс у гамбургера
-       mainNavbar.classList.remove('activeNavbar'); 
-   }
+    if (mainNavbar.classList.contains('activeNavbar')) {
+        // код удаляющий активный класс у гамбургера
+        mainNavbar.classList.remove('activeNavbar');
+    }
 })
 
 const closeHamburger = document.querySelector('.nav-close');
 closeHamburger.addEventListener('click', () => {
     mainNavbar.classList.remove('activeNavbar');
-}); 
+});
 
 
 // sort img 
 
-//============================================================
-
-/* const portfolioImgs = document.querySelectorAll('.portfolio-image');
-
-    portfolioImgs.forEach((elem, index) => {
-    console.log(elem, elem.alt);
-}); */
 
 
- console.log(document.querySelector('h2').innerText);
- //.forEach((elem) => console.log(elem.innerHTML));
+const portfolioGroupBtnContainer = document.querySelector('.portfolio-group-btn');
+const portfolioGroupBtn = document.querySelectorAll('.btn-portfolio');
+
+
+portfolioGroupBtnContainer.addEventListener('click', (event) => {
+    let button = event.target;
+    let timeOfTheYear = button.dataset;
+
+    //работаем с кнопками перебирая их и меняем bg
+
+    portfolioGroupBtn.forEach(elem => {
+        if(elem.classList.contains('btn-active')) {
+            elem.classList.remove('btn-active');
+        }
+    });
+
+    if (button.classList.contains('btn-portfolio')) {
+        button.classList.add('btn-active');
+    }
+    
+    // работаем с изображениями
+    
+});
+
