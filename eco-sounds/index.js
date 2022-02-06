39dc12f28a27e52106a1de9b7a984336218d50a0
Andrew Pisliakov <pislakov@gmail.com>
2022-02-06 22:01:05 +0300

    feat: change audio

--- eco-sounds/index.js
+++ eco-sounds/index.js
@@ -1,5 +1,6 @@
 const headerContainer = document.querySelector('.header-container');
 const elemsNavItems = document.querySelectorAll('.nav-item');
+let currentButtonBird = 'forest';
 
 //console.log( headerContainer );
 //console.log( elemsNavItems );
@@ -23,10 +24,62 @@ headerContainer.addEventListener('click', (event) => {
     // работаем с изображениями 
     if (currentElem.classList.contains('nav-item')) {
         let img = document.querySelector('.main-container');
-        let currentButtonBird = currentElem.dataset.bird;
-        
+        currentButtonBird = currentElem.dataset.bird;
+        console.log(currentButtonBird);
+
         img.style.backgroundImage = `url(../assets/img/${currentButtonBird}.jpg)`
+        audioPlayer();
     }
 });
 
 
+// изменение кнопки play/pause 
+const btnPlay = document.querySelector('.btn-play');
+btnPlay.addEventListener('click', changeClass);
+
+function changeClass() {
+    btnPlay.classList.toggle('pause')
+}
+
+btnPlay.addEventListener('click', audioPlayer);
+
+// проигрыватель 
+let isPlay = false;
+const audio = document.querySelector('audio');
+//console.log(audio.src);
+
+//audio.src = `./assets/audio/${currentButtonBird}.mp3`;
+//console.log(audio.srs);
+
+function playAudio() {
+    audio.play();
+}
+
+function pauseAudio() {
+    audio.pause();
+}
+
+function audioPlayer() {
+    audio.src = `./assets/audio/${currentButtonBird}.mp3`;
+    console.log(audio.src);
+    console.log(currentButtonBird);
+   
+    if (!isPlay) {
+        playAudio();
+        isPlay = true;
+    } else {
+        pauseAudio();
+        isPlay = false;
+
+    }
+    console.log(isPlay);
+}
+console.log(isPlay);
+
+//audioPlayer();
+
+//setInterval(() => console.log(currentButtonBird, typeof currentButtonBird, 'currentButtonBird', audio.src ), 3000);
+//console.log(currentButtonBird, 'currentButtonBird');
+
+
+//<audio src="./assets/audio/drozd.mp3" controls></audio>
