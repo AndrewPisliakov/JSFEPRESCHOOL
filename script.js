8a8893e142c64e93ef459a568b6134f318345716
Andrew Pisliakov <pislakov@gmail.com>
2022-02-14 18:48:02 +0300

    feat: add search with button

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const input = document.querySelector('#input');
+const buttonClose = document.querySelector('.close');
+const buttonSearch = document.querySelector('#search');
 const divContainer = document.querySelector('.main-container');
 
 class Storage {
@@ -8,7 +10,7 @@ class Storage {
     setItem(data) { localStorage.setItem(this._key, JSON.stringify(data)) }
     removeItem() { localStorage.removeItem(this._key) }
     getItem() { return JSON.parse(localStorage.getItem(this._key)) }
-    claerAllItem() { localStorage.clear() }
+    clareAllItem() { localStorage.clear() }
 };
 
 let storage = new Storage('request');
@@ -17,17 +19,11 @@ if (storage.getItem()) {
     input.value = storage.getItem();
 }
 
-function generateUrl(keyword) {
-    if (keyword) {
-        return `https://api.unsplash.com/search/photos?query=${keyword}&per_page=30&orientation=landscape&client_id=XAdjixWcAbF3mw0hUjTkyY7tgpGEPSSUc_Gj7t0hXjw`;
-    } else {
-        return `https://api.unsplash.com/search/photos?query=${keyword = 'spring'}&per_page=30&orientation=landscape&client_id=XAdjixWcAbF3mw0hUjTkyY7tgpGEPSSUc_Gj7t0hXjw`;
-    }
-}
-
-input.addEventListener('input', function () {
-    localStorage.setItem(this.value);
-});
+//=========================================================================================
+async function loadImgs() {
+    let arrObjImgs = await getImages(generateUrl(input.value));
+    displayImgs(arrObjImgs);
+};
 
 async function getImages(url) {
     try {
@@ -52,6 +48,21 @@ function displayImgs(arrObj) {
     });
 };
 
+function generateUrl(keyword) {
+    if (keyword) {
+        return `https://api.unsplash.com/search/photos?query=${keyword}&per_page=30&orientation=landscape&client_id=XAdjixWcAbF3mw0hUjTkyY7tgpGEPSSUc_Gj7t0hXjw`;
+    } else {
+        return `https://api.unsplash.com/search/photos?query=${keyword = 'spring'}&per_page=30&orientation=landscape&client_id=XAdjixWcAbF3mw0hUjTkyY7tgpGEPSSUc_Gj7t0hXjw`;
+    }
+}
+
+//=======================================================================================
+window.addEventListener('load', loadImgs);
+
+input.addEventListener('input', function () {
+    storage.setItem(this.value);
+});
+
 input.addEventListener('keyup', (event) => {
     event.preventDefault();
     if (event.keyCode === 13) {
@@ -59,9 +70,30 @@ input.addEventListener('keyup', (event) => {
     }
 });
 
-window.addEventListener('load', loadImgs);
+buttonSearch.addEventListener('click', loadImgs);
 
-async function loadImgs() {
-    let arrObjImgs = await getImages(generateUrl(input.value));
-    displayImgs(arrObjImgs);
-};
+buttonClose.addEventListener('click', function() {
+    input.value = '';
+    storage.clareAllItem();
+});
+ 
+
+//============================================================================================
+console.log(`
+- на странице есть несколько фото и строка поиска +5;
+- в футере приложения есть ссылка на гитхаб автора приложения, год создания приложения, логотип курса со ссылкой на курс +5;
+- При загрузке приложения на странице отображаются полученные от API изображения +10;
+- Если в поле поиска ввести слово и отправить поисковый запрос, на странице отобразятся изображения соответствующей тематики, если такие данные предоставляет API +10;
+- при открытии приложения курсор находится в поле ввода +5;
+- есть placeholder +5;
+- автозаполнение поля ввода отключено (нет выпадающего списка с предыдущими запросами) +5;
+- поисковый запрос можно отправить нажатием клавиши Enter +5;
+- после отправки поискового запроса и отображения результатов поиска, поисковый запрос продолжает отображаться в поле ввода +5;
+- в поле ввода есть крестик при клике по которому поисковый запрос из поля ввода удаляется и отображается placeholder +5;
+
+Оценка за задание 60 баллов
+`);
+
+
+
+
