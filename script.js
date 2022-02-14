a4ac83164cb14c3b22e70faf6f8dbc7d1bf41690
Andrew Pisliakov <pislakov@gmail.com>
2022-02-14 17:14:52 +0300

    feat: add storage for Class

--- script.js
+++ script.js
@@ -1,53 +1,47 @@
+const input = document.querySelector('#input');
+const divContainer = document.querySelector('.main-container');
 
-let input = document.querySelector('#input');
-let request = input.value;
-let url = `https://api.unsplash.com/search/photos?query=${request = 'winter'}&per_page=30&orientation=landscape&client_id=XAdjixWcAbF3mw0hUjTkyY7tgpGEPSSUc_Gj7t0hXjw`;
-let divContainer = document.querySelector('.main-container');
-
-
-
-
-if (localStorage.getItem('isRequest')) {
-    request = JSON.parse(localStorage.getItem('isRequest'));
-    input.value = request;
-    url = `https://api.unsplash.com/search/photos?query=${request}&per_page=30&orientation=landscape&client_id=XAdjixWcAbF3mw0hUjTkyY7tgpGEPSSUc_Gj7t0hXjw`;
-    console.log(request.length);
-}
-
-
-
-//после отправки поискового запроса и отображения результатов поиска, поисковый запрос 
-//продолжает отображаться в поле ввода +5
-//=====================================================
-
-input.addEventListener('input', function () {
-
-    request = this.value;
-    console.log(request);
-
-    url = `https://api.unsplash.com/search/photos?query=${request}&per_page=30&orientation=landscape&client_id=XAdjixWcAbF3mw0hUjTkyY7tgpGEPSSUc_Gj7t0hXjw`;
-
-    localStorage.setItem('isRequest', JSON.stringify(request));
-});
-
-console.log(request);
-
-
-setInterval(() => { console.log(request, url) }, 5000);
-
-async function getImages(url) {
-
-    let response = await fetch(url);
-    let data = await response.json();
-    return data.results;
+class Storage {
+    constructor(key) {
+        this._key = key;
+    }
+    setItem(data) { localStorage.setItem(this._key, JSON.stringify(data)) }
+    removeItem() { localStorage.removeItem(this._key) }
+    getItem() { return JSON.parse(localStorage.getItem(this._key)) }
+    claerAllItem() { localStorage.clear() }
 };
 
+let storage = new Storage('request');
+
+if (storage.getItem()) {
+    input.value = storage.getItem();
+}
+
+function generateUrl(keyword) {
+    if (keyword) {
+        return `https://api.unsplash.com/search/photos?query=${keyword}&per_page=30&orientation=landscape&client_id=XAdjixWcAbF3mw0hUjTkyY7tgpGEPSSUc_Gj7t0hXjw`;
+    } else {
+        return `https://api.unsplash.com/search/photos?query=${keyword = 'spring'}&per_page=30&orientation=landscape&client_id=XAdjixWcAbF3mw0hUjTkyY7tgpGEPSSUc_Gj7t0hXjw`;
+    }
+}
+
+input.addEventListener('input', function () {
+    localStorage.setItem(this.value);
+});
+
+async function getImages(url) {
+    try {
+        let response = await fetch(url);
+        let data = await response.json();
+
+        return data.results;
+    } catch (error) {
+        console.log(error);
+    }
+};
 
 function displayImgs(arrObj) {
-
     divContainer.innerHTML = '';
-    console.log(arrObj);
-
 
     arrObj.forEach((obj) => {
         let url = obj.urls.regular;
@@ -56,19 +50,11 @@ function displayImgs(arrObj) {
         div.style.backgroundImage = `url(${url})`;
         divContainer.append(div);
     });
-
-}
-
-
-
-// поисковый запрос можно отправить нажатием клавиши Enter 
-//==========================================================
-
+};
 
 input.addEventListener('keyup', (event) => {
     event.preventDefault();
     if (event.keyCode === 13) {
-        console.log('Do something', url);
         loadImgs();
     }
 });
@@ -76,10 +62,6 @@ input.addEventListener('keyup', (event) => {
 window.addEventListener('load', loadImgs);
 
 async function loadImgs() {
-    let arrObjImgs = await getImages(url);
+    let arrObjImgs = await getImages(generateUrl(input.value));
     displayImgs(arrObjImgs);
 };
-
-
-
-
